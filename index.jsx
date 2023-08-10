0e37157ba742a196b4e18b70520375b1223493a1
Portian02 <135663498+Portian02@users.noreply.github.com>
2023-08-10 13:40:19 -0600

    nombre

--- index.jsx
+++ index.jsx
@@ -12,7 +12,7 @@ const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const initialValue = 0;
 const sumaTotal= numeros.reduce((acumulator, currentValue)=>acumulator + currentValue, initialValue)
 console.log(sumaTotal);
-
+//_________________________Sebas__________________\\
 const numbers = [54, 64, 75, 89, 90, 23, 45];
 
 const reduceTotal = (accumulator, currentV) => {
